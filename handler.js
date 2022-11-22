5afbb2e84a4004b688c42cef5c0814f6cd4ebc0d
Henrique Fuschini <henriquefuschini@kissolutions.tech>
2022-11-22 17:35:29 -0600

    update handler with new logic using ec2

--- handler.js
+++ handler.js
@@ -1,34 +1,106 @@
 const serverless = require("serverless-http");
 const { exec } = require("child_process");
 const express = require("express");
+const SSH = require('simple-ssh');
+const { v4: uuidv4 } = require('uuid');
+const bodyParser = require('body-parser');
+const fs = require("fs");
 const app = express();
 
-process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT']
+app.use(bodyParser.json({ strict: false }));
 
-app.get("/", (req, res, next) => {
+function getSessionId() {
+  return uuidv4();
+}
 
-  exec("./Emojicode-1.0-beta.2-Darwin-x86_64/emojicodec test.emojic && ./test", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return res.status(500).json({
-          message: `error: ${error.message}`,
-        });
-      }
-    if (stderr) {
-      return res.status(500).json({
-        message: `stderr: ${stderr}`,
-      });
-    }
-    return res.status(200).json({
-      message: `stdout: ${stdout}`,
-    });
-  });
-});
-
-app.get("/hello", (req, res, next) => {
+app.post("/test", async (req, res, next) => {
+  console.log(req.body);
   return res.status(200).json({
-    message: "Hello from path!",
+    message: req.body,
   });
+})
+
+app.post("/ec2", async (req, res, next) => {
+  
+  if (!req.body.code) {
+    return res.status(400).json({
+      message: "Missing parameter code",
+    });
+  }
+  const input = req.body.code
+  console.log("starting session with code:");
+  console.log(input);
+
+  const sessionId = getSessionId()
+  console.log(`sessionId: ${sessionId}`);
+  
+  const pemfile = 'emojicode_ec2.pem';
+  const user = 'ec2-user';
+  const host = 'ec2-54-205-146-180.compute-1.amazonaws.com';
+
+  const ssh = new SSH({
+    host: host,
+    user: user,
+    key: fs.readFileSync(pemfile)
+  });
+
+  let prom = new Promise(function(resolve, reject) {
+
+    let output = {
+      logs: '',
+      output: ''
+    };
+
+    ssh.exec(`echo "${input}" > ./input/${sessionId}.emojic`, {
+      err: function(stderr) {
+        output.logs += stderr;
+      },
+      out: function(stdout) {
+        output.logs += stdout;
+      }
+    }).exec(`emojicodec ./input/${sessionId}.emojic`, {
+      out: function(stdout) {
+        output.logs += stdout;
+      },
+      err: function(stderr) {
+        output.logs += stderr;
+      }
+    }).exec(`./input/${sessionId}`, {
+      out: function(stdout) {
+        output.logs += stdout;
+        output.output += stdout;
+      },
+      err: function(stderr) {
+        output.logs += stderr;
+      }
+    }).exec(`rm input/${sessionId} && rm input/${sessionId}.o`, {
+      exit: function() {
+        resolve(output);
+      },
+      out: function(stdout) {
+        output.logs += stdout;
+      },
+      err: function(stderr) {
+        output.logs += stderr;
+      }
+    }).start({
+      success: function() {
+        console.log("successful connection!");
+      },
+      fail: function(e) {
+        console.log("failed connection, boo");
+        console.log(e);
+      }
+    });
+
+  });
+
+  const response = await prom;
+
+  return res.status(200).json({
+    message: response,
+  });
+  
 });
 
 app.use((req, res, next) => {
